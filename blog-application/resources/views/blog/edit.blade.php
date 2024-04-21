675b3a58612e8f2515610602e894f165e64fc4db
Sourav Majumder <souravmajumderk99@gmail.com>
2024-04-21 18:07:14 +0600

    (feature)[blade]: Learn how to create blade layout and how to use css

--- blog-application/resources/views/blog/edit.blade.php
+++ blog-application/resources/views/blog/edit.blade.php
@@ -1,8 +1,37 @@
-{{-- foldername.filename --}}
+{{-- layoute --}}
+{{-- foldername.filename
 @extends("component.layout")
 
 @section("title", "My Simple web application")
 
 @section("content")
 <h1>Index layout system baby</h1>
-@endsection
+@endsection --}}
+
+
+{{-- component --}}
+<x-layout>
+    <div>
+        <div class="container">
+            <h2>Update Blog</h2>
+            <form method="post">
+                {{-- @method("PUT")
+                @csrf --}}
+                <div class="form-group">
+                    <label for="title">Title:</label>
+                    <input type="text" id="title" name="title" value="Sample Title" placeholder="Enter title">
+                </div>
+                <div class="form-group">
+                    <label for="description">Description:</label>
+                    <textarea id="description" name="description"
+                        placeholder="Enter description">
+                        {{-- {{ $blog->description }} --}}
+                    </textarea>
+                </div>
+                <button type="submit" class="form-btn">Update Blog</button>
+            </form>
+            <br>
+            {{-- <a href="{{ route('blog.index') }}" class="action-link view-link">Back</a> --}}
+        </div>
+    </div>
+</x-layout>
--- blog-application/resources/views/blog/show.blade.php
+++ blog-application/resources/views/blog/show.blade.php
@@ -1,8 +1,33 @@
-{{-- foldername.filename --}}
+{{-- layoute --}}
+{{-- foldername.filename
 @extends("component.layout")
 
 @section("title", "My Simple web application")
 
 @section("content")
 <h1>Index layout system baby</h1>
-@endsection
+@endsection --}}
+
+
+{{-- component --}}
+<x-layout>
+   <div>
+        <div class="container">
+            <h2>Blog Details</h2>
+            <div class="blog-details">
+                <h3>Title:</h3>
+                {{-- <p>{{ $blog->title }}</p> --}}
+            </div>
+            <div class="blog-details">
+                <h3>Description:</h3>
+                {{-- <p>{{ $blog->description }}</p> --}}
+            </div>
+            <div class="blog-details">
+                <h3>Created At:</h3>
+                {{-- <p>{{ $blog->created_at }}</p> --}}
+            </div>
+            {{-- <a href="{{ route('blog.index') }}" class="action-link view-link">Back</a>
+            <a href="{{ route('blog.edit', $blog) }}" class="action-link edit-link">Edit</a> --}}
+        </div>
+    </div>
+</x-layout>
