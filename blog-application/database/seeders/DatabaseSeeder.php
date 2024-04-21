c11f272032ec03f52ecb66a67e0fa08caa231186
Sourav Majumder <souravmajumderk99@gmail.com>
2024-04-21 16:33:12 +0600

    (feature)[routes]: Add blogcontroller with blog model

--- blog-application/database/seeders/DatabaseSeeder.php
+++ blog-application/database/seeders/DatabaseSeeder.php
@@ -3,21 +3,26 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Blog;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
+    public function run(): void {
         // User::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            "id" => 1,
+            "password" => bcrypt("password")
         ]);
+
+        // Blogs data
+        // how much data you wanna  generate pass that value in the parameters like 100
+        Blog::factory(100)->create();
     }
 }
--- blog-application/routes/web.php
+++ blog-application/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Sample1Controller;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// resource route
+Route::resource("sample1", Sample1Controller::class);
